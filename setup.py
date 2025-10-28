7c5da0730af19ac2910a32df289bdbbd4d254dd5
Prakhar Shukla <SmokenX1@gmail.com>
2025-10-28 16:11:36 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="py_yt_search",
-    version="0.3.1",
+    version="0.3",
     packages=find_packages(),
     install_requires=[],
     author="Prakhar",
