ddf636feb4df01ad196d772bf36cc9a6264495bc
Prakhar Shukla <SmokenX1@gmail.com>
2025-10-28 15:57:10 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="py_yt_search",
+    version="0.3.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Prakhar",
+    description="Advanced YouTube search library Without Data-V3 API Key",
+    python_requires=">=3.8",
+)
